93e30753cbf70c7de204794ef6128d477aa1e1b2
Nathan Huckabay <nathanhuckabay@gmail.com>
2022-12-10 10:41:33 -0600

    basic index file created

---
+++ static/scripts/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        
+        
+    },
+    created: function() {    
+    },
+    mounted(){
+    }
+})
