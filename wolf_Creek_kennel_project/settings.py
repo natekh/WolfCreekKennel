42e66a534fb1f11fc0a951938454f950d28e79cc
Nathan Huckabay <nathanhuckabay@gmail.com>
2022-12-21 17:35:49 -0600

    heroku

--- wolf_Creek_kennel_project/settings.py
+++ wolf_Creek_kennel_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from .config import django_key
+#from .config import django_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
